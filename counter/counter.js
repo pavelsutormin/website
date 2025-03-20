91328522ff5e9c9ed6b2839967b3e08b26fe8138
pavelsutormin <101486519+pavelsutormin@users.noreply.github.com>
2025-03-20 10:19:40 -0700

    Add multiple pages

--- counter/counter.js
+++ counter/counter.js
@@ -1,19 +1,55 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, onValue } from "firebase/database";
 import '/style.css'
 
 document.querySelector('#app').innerHTML = `
+<div class="navbar">
+    <a class="navbar-item" href="/">Home</a>
+    <a class="navbar-item active" href="/counter/">Counter</a>
+</div>
 <div>
     <h1>This is a counter!</h1>
+    <h3>Everybody on this page can click it and they will all see 1 added.</h3>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
 </div>
 `
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDISkQ8lbrwGlpVrhRKkrce1V9ASil02-Q",
+    authDomain: "pasha-website.firebaseapp.com",
+    databaseURL: "https://pasha-website-default-rtdb.firebaseio.com",
+    projectId: "pasha-website",
+    storageBucket: "pasha-website.firebasestorage.app",
+    messagingSenderId: "196521440659",
+    appId: "1:196521440659:web:0ee764053e7080e6dce466",
+    measurementId: "G-5LPWDB0P15"
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const counterRef = ref(db, "counter");
+
 const counter = document.querySelector('#counter');
 let count = 0
 const setCounter = (new_count) => {
-    count = new_count
-    counter.innerHTML = `count is ${count}`
-}
+    count = new_count;
+    counter.innerHTML = `count is ${count}`;
+
+    set(counterRef, {
+        count: count
+    }).then(r => console.log(r));
+};
 counter.addEventListener('click', () => setCounter(count + 1))
-setCounter(0)
+onValue(counterRef, (snapshot) => {
+    if (snapshot.exists()) {
+        const data = snapshot.val();
+        setCounter(data["count"])
+        console.log("Data changed:", data);
+
+    } else {
+        console.log("No data available");
+    }
+}, (error) => {
+    console.error("Error fetching data:", error);
+});
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-import '/style.css'
+import './style.css'
 
 document.querySelector('#app').innerHTML = `
+<div class="navbar">
+    <a class="navbar-item active" href="/">Home</a>
+    <a class="navbar-item" href="/counter/">Counter</a>
+</div>
 <div>
     <h1>This is my website</h1>
-    <a href="/counter/">Counter Test Site</a>
 </div>
 `
