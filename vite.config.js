25774d094a9bf4c95715e14da8c73867908d7fc0
pavelsutormin <101486519+pavelsutormin@users.noreply.github.com>
2025-03-21 18:21:03 -0700

    Add multiple pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 counter: resolve(__dirname, 'counter/index.html'),
+                bml_maker: resolve(__dirname, 'bml_maker/index.html'),
+                data_uri_maker: resolve(__dirname, 'data_uri_maker/index.html'),
+                bm_collect: resolve(__dirname, 'bm_collect/index.html'),
+                lorem: resolve(__dirname, 'lorem/index.html'),
+                blank: resolve(__dirname, 'blank/index.html'),
             },
         },
     },
